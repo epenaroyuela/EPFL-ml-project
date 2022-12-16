e50769c0047f45ae465cecf8dfe8dc277dd2771e
aitor1ganuza <aitor.ganuza.izagirre@gmail.com>
2022-12-16 19:57:04 +0100

    fix typo

--- src/evaluation.py
+++ src/evaluation.py
@@ -25,7 +25,7 @@ def distance():
 
     def _inner(tl, pl):
         def manhattan_dist(p1, p2):
-            return abs((p1[0] - p2[0]) + (p1[1] - p2[1]))
+            return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
 
         return manhattan_dist(tl, pl)
 
